3edf7a8ca0300692d48f8c21201ad5a6d0a3cf23
Bhavani.Kurru <Bhavani.Kurru@mlcinsurance.com.au>
2021-03-19 06:23:54 +1100

    added header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepages/homepage.component';
 import {Switch,Route,Link} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 const HatsPage = (props) =>{
@@ -24,11 +25,12 @@ const HatsPage = (props) =>{
 function App() {
   return (
     <div>
-      <Link to='/shop/hats'>HATS</Link>
+      <Header></Header>
+      <Switch>
       <Route  exact path='/' component={HomePage}></Route> 
       <Route exact path='/shop/hats' component={HatsPage}></Route>
       <Route exact path='/shop/' component={ShopPage}></Route>
-
+      </Switch>
     </div>
   );
 }
