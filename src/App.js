501f3df67de3061b4961b219902442e742828e35
Bhavani.Kurru <Bhavani.Kurru@mlcinsurance.com.au>
2021-03-21 15:09:18 +1100

    sign in

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepages/homepage.component';
 import {Switch,Route,Link} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 const HatsPage = (props) =>{
@@ -30,6 +31,7 @@ function App() {
       <Route  exact path='/' component={HomePage}></Route> 
       <Route exact path='/shop/hats' component={HatsPage}></Route>
       <Route exact path='/shop/' component={ShopPage}></Route>
+      <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
